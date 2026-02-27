1586400c44f316644e57b5a9fa5d03a06f1a5f9c
integradaneuropsicologia <integradaneuropsicologia@gmail.com>
2026-02-27 20:26:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ async function dbPatchBy(table, column, value, patch) {
 }
 
 
-const PATIENT_PORTAL_URL = "https://integradaneuropsicologia.github.io/sistema-de-cadastro-de-formularios-v.2.0/";
+const PATIENT_PORTAL_URL = "https://integradaneuropsicologia.github.io/area-do-paciente-v2/";
 
 /* ===== HELPERS DOM ===== */
 const $ = (s) => document.querySelector(s);
@@ -2020,5 +2020,6 @@ async function doLogin() {
   enterLookupMode();
   $("#pacCPF").focus();
 }
+
 
 
